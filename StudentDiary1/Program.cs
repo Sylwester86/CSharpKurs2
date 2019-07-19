073207b3aed96c19182c8d7f84b66979bce81b30
Sylwek <Sylwek@SYLWEK-PC>
2019-07-19 14:16:13 +0200

    "Moja pierwsza klasa koniec zadania"

--- StudentDiary1/Program.cs
+++ StudentDiary1/Program.cs
@@ -10,23 +10,44 @@ namespace StudentDiary1
         static void Main(string[] args)
         {
             Diary diary = new Diary();
-            diary.AddRating(5);
-            diary.AddRating(8.5f);
-            diary.AddRating(4.7f);
+            //diary.AddRating(5);
+            //diary.AddRating(8.5f);
+            //diary.AddRating(4.7f);
 
-            float avg = diary.CalculateAverage();
-            float max = diary.GiveMaxRating();
-            float mix = diary.GiveMinRating();
+            //float avg = diary.CalculateAverage();
+            //float max = diary.GiveMaxRating();
+            //float mix = diary.GiveMinRating();
 
             for (; ; )
             {
-                Console.WriteLine(" Podaj Ocenę z zakresy 1-10");
+                Console.WriteLine(" Podaj Ocenę z zakresu 1-10");
 
-                float rating = float.Parse(Console.ReadLine());
+                float rating;
+                bool result = float.TryParse(Console.ReadLine(), out rating);
 
+                if (rating == 11)
+                {
+                    break;
+                }
 
-
+                if (result)
+                {
+                    if (rating > 0 && rating <= 10)
+                    {
+                        diary.AddRating(rating);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Niepoprawna liczba, podaj liczbe z zakresu 1-10 ");
+                    }
+                }
             }
-        }
+
+            Console.WriteLine("Średnia twoich ocen to : " + diary.CalculateAverage());
+            Console.WriteLine("Najwyższa ocena to : " + diary.GiveMaxRating());
+            Console.WriteLine("Najniższa ocena to : " + diary.GiveMinRating());
+            Console.ReadKey();
+         }  
+
     }
 }
